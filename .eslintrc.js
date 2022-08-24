a6b873bfbd4a6aa2a69b25cc83d5d5d0310785fb
Kiernan Soriano <ksoriano10@horizon.csueastbay.edu>
2022-08-24 13:19:39 -0700

    Install and setup eslint/prettier

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["plugin:react/recommended", "airbnb", "prettier"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "prettier", "prefer-arrow"],
+  rules: {
+    "prettier/prettier": "error",
+    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
+    "prefer-arrow/prefer-arrow-functions": [
+      "error",
+      {
+        disallowPrototype: true,
+        singleReturnOnly: false,
+        classPropertiesAllowed: false,
+      },
+    ],
+    "prefer-arrow-callback": ["error", { allowNamedFunctions: true }],
+    "func-style": ["error", "expression"],
+    "react/function-component-definition": [
+      2,
+      {
+        namedComponents: "arrow-function",
+        unnamedComponents: "arrow-function",
+      },
+    ],
+  },
+};
