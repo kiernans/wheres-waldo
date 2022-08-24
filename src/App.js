efc65b4521c19e4159108ea62e75d6c64802f2c7
Kiernan Soriano <ksoriano10@horizon.csueastbay.edu>
2022-08-23 17:42:09 -0700

    Add Routing to App, remove margins and padding globally

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import './App.css';
+import React from "react";
+import { MainPage } from "./routes/MainPage";
+import { Routes, Route } from "react-router-dom";
+import { Level } from "./routes/Level";
+import level1 from './assets/2687207.jpg'
+import './styles/App.css'
 
 function App() {
+
+  // eslint-disable-next-line
+  const getCoordinates = (e) => {
+    const x = e.pageX - e.target.offsetLeft;
+    const y = e.pageY - e.target.offsetTop;
+    console.log(x, y);
+  };
+
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <Routes>
+        <Route path='/'
+               element={<MainPage />}/>
+          <Route path='level1' 
+                  element={<Level image={level1}
+                                  level={1}
+                                  characters={'waldo'}/>}/>
+        <Route path='*' element={<h1>Nothing here!</h1>}/>
+      </Routes>
+    </>
   );
 }
 
