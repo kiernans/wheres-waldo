43600d7c980deea97e443e275b4e0acaa06c4382
Kiernan Soriano <ksoriano10@horizon.csueastbay.edu>
2022-08-24 13:20:33 -0700

    Remove logic from App

--- src/App.js
+++ src/App.js
@@ -1,32 +1,19 @@
 import React from "react";
-import { MainPage } from "./routes/MainPage";
 import { Routes, Route } from "react-router-dom";
-import { Level } from "./routes/Level";
-import level1 from './assets/2687207.jpg'
-import './styles/App.css'
+import MainPage from "./routes/MainPage";
+import Level from "./routes/Level";
+import level1 from "./assets/2687207.jpg";
+import "./styles/App.css";
 
-function App() {
-
-  // eslint-disable-next-line
-  const getCoordinates = (e) => {
-    const x = e.pageX - e.target.offsetLeft;
-    const y = e.pageY - e.target.offsetTop;
-    console.log(x, y);
-  };
-
-  return (
-    <>
-      <Routes>
-        <Route path='/'
-               element={<MainPage />}/>
-          <Route path='level1' 
-                  element={<Level image={level1}
-                                  level={1}
-                                  characters={'waldo'}/>}/>
-        <Route path='*' element={<h1>Nothing here!</h1>}/>
-      </Routes>
-    </>
-  );
-}
+const App = () => (
+  <Routes>
+    <Route path="/" element={<MainPage />} />
+    <Route
+      path="level1"
+      element={<Level image={level1} level={1} characters="waldo" />}
+    />
+    <Route path="*" element={<h1>Nothing here!</h1>} />
+  </Routes>
+);
 
 export default App;
