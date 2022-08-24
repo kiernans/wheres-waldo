90d28789a7dbad014012231b77f22e192a50c32b
Kiernan Soriano <ksoriano10@horizon.csueastbay.edu>
2022-08-24 13:20:05 -0700

    Firebase edit

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { db } from '@firebase/firestore';
+import { getFirestore } from "@firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -11,10 +11,10 @@ const firebaseConfig = {
   projectId: "where-s-waldo-1cc9c",
   storageBucket: "where-s-waldo-1cc9c.appspot.com",
   messagingSenderId: "485989359098",
-  appId: "1:485989359098:web:3ebdda1b743f4d1e0f9843"
+  appId: "1:485989359098:web:3ebdda1b743f4d1e0f9843",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-export const db = getFireStore(app);
+export const db = getFirestore(app);
