35681557b396846a4f8e70ab476e66440d12e0f2
Kiernan Soriano <ksoriano10@horizon.csueastbay.edu>
2022-08-24 19:22:42 -0700

    Add proptypes to level

--- src/routes/Level.js
+++ src/routes/Level.js
@@ -1,14 +1,37 @@
 import React from "react";
+import PropTypes from "prop-types";
 import LevelNavbar from "../components/LevelNavbar";
+import useLevelHelper from "../hooks/useLevelHelper";
 import "../styles/Level.css";
 
-const Level = ({ image, level, characters }) => (
-  <>
-    <LevelNavbar />
-    <div className="level-image">
-      <img src={image} alt="" />
-    </div>
-  </>
-);
+const Level = ({ image, level, characters }) => {
+  const {
+    getCoordinatesFromImage,
+    answer,
+    compareAnswerCoordinatesWithChoice,
+  } = useLevelHelper();
+
+  const handleOnClick = (e) => {
+    const choice = getCoordinatesFromImage(e);
+    console.log(compareAnswerCoordinatesWithChoice(answer, choice));
+  };
+
+  return (
+    <>
+      <LevelNavbar />
+      <div className="level-image">
+        <img src={image} alt="" onClick={handleOnClick} aria-hidden="true" />
+        {level}
+        {characters}
+      </div>
+    </>
+  );
+};
+
+Level.propTypes = {
+  image: PropTypes.string.isRequired,
+  level: PropTypes.number.isRequired,
+  characters: PropTypes.string.isRequired,
+};
 
 export default Level;
