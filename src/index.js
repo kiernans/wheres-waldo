2764724db6fd16303ea196e539784ff536a1c3b7
Kiernan Soriano <ksoriano10@horizon.csueastbay.edu>
2022-08-24 19:23:17 -0700

    Edit App

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
@@ -11,5 +11,3 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
-
-
