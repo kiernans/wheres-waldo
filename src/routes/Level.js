7ab2c88579a7d8014c32d1103472d88490b5bbb6
Kiernan Soriano <ksoriano10@horizon.csueastbay.edu>
2022-08-24 20:58:36 -0700

    Create choice state and update within useEffect

--- src/routes/Level.js
+++ src/routes/Level.js
@@ -1,20 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import LevelNavbar from "../components/LevelNavbar";
 import useLevelHelper from "../hooks/useLevelHelper";
 import "../styles/Level.css";
+import Dropdown from "../components/Dropdown";
 
-const Level = ({ image, level, characters }) => {
+const Level = ({ image }) => {
+  const [choice, setChoice] = useState({ x: 0, y: 0 });
+
   const {
     getCoordinatesFromImage,
     answer,
     compareAnswerCoordinatesWithChoice,
   } = useLevelHelper();
 
+  const { id, level, names } = answer;
+
   const handleOnClick = (e) => {
-    const choice = getCoordinatesFromImage(e);
-    console.log(compareAnswerCoordinatesWithChoice(answer, choice));
+    setChoice(getCoordinatesFromImage(e));
   };
+
+  useEffect(() => {
+    console.log(choice);
+    console.log(compareAnswerCoordinatesWithChoice(answer, choice));
+  }, [choice]);
 
   return (
     <>
@@ -22,16 +31,14 @@ const Level = ({ image, level, characters }) => {
       <div className="level-image">
         <img src={image} alt="" onClick={handleOnClick} aria-hidden="true" />
         {level}
-        {characters}
       </div>
+      <Dropdown id={id} names={names} choice={choice} />
     </>
   );
 };
 
 Level.propTypes = {
   image: PropTypes.string.isRequired,
-  level: PropTypes.number.isRequired,
-  characters: PropTypes.string.isRequired,
 };
 
 export default Level;
