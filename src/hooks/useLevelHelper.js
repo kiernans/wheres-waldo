cb065ab0063197b42a49be7a9446b8791f75ca51
Kiernan Soriano <ksoriano10@horizon.csueastbay.edu>
2022-08-26 10:33:18 -0700

    Create characters array for Dropdown

--- src/hooks/useLevelHelper.js
+++ src/hooks/useLevelHelper.js
@@ -1,10 +1,12 @@
 const useLevelHelper = () => {
   const answer = {
     id: 1,
-    names: ["waldo", "test", "test2"],
     level: 1,
-    x: 1411,
-    y: 483,
+    characters: [
+      { id: 1, name: "waldo", x: 1411, y: 483 },
+      { id: 2, name: "test", x: 0, y: 0 },
+      { id: 3, name: "test2", x: 1, y: 1 },
+    ],
   };
 
   const getCoordinatesFromImage = (e) => {
@@ -14,11 +16,19 @@ const useLevelHelper = () => {
     return { x, y };
   };
 
-  const compareAnswerCoordinatesWithChoice = (fAnswer, choice) => {
-    const { x: xAnswer, y: yAnswer } = fAnswer;
+  const getCharacterFromAnswer = (array, choiceName) =>
+    array.filter((character) => character.name === choiceName)[0];
+
+  const compareAnswerCoordinatesWithChoice = (choice, choiceName) => {
+    const character = getCharacterFromAnswer(answer.characters, choiceName);
+    const { x: xAnswer, y: yAnswer } = character;
     const { x: xChoice, y: yChoice } = choice;
 
-    if (Math.abs(xAnswer - xChoice) < 30 && Math.abs(yAnswer - yChoice) < 30)
+    if (
+      Math.abs(xAnswer - xChoice) < 30 &&
+      Math.abs(yAnswer - yChoice) < 30 &&
+      choiceName === character.name
+    )
       return true;
 
     return false;
