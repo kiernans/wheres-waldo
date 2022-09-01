1c5f007a44ed725589d73737eaf79f887a588eef
Kiernan Soriano <ksoriano10@horizon.csueastbay.edu>
2022-08-31 20:44:11 -0700

    Move levels array into its own file

---
+++ src/assets/levels.js
@@ -0,0 +1,9 @@
+export const levels = {
+  id: 1,
+  level: 1,
+  characters: [
+    { id: 1, name: "waldo", x: 1411, y: 483 },
+    { id: 2, name: "test", x: 0, y: 0 },
+    { id: 3, name: "test2", x: 1, y: 1 },
+  ],
+};
