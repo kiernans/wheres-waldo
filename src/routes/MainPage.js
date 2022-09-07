815ffad427f3ee46fd7f8ad957c994fb917be0b0
Kiernan Soriano <ksoriano10@horizon.csueastbay.edu>
2022-09-07 15:09:46 -0700

    Add Navbar and Level icons to Mainpage

--- src/routes/MainPage.js
+++ src/routes/MainPage.js
@@ -1,11 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import level1 from "../assets/level1-small.jpg";
+import level2 from "../assets/level2-small.jpg";
+import level3 from "../assets/level3-small.jpg";
+import MainNavbar from "../components/MainNavbar";
+import "../styles/MainPage.css";
 
 const MainPage = () => (
   <>
-    <Link to="level1">Level 1</Link>
-    <Link to="level2">Level 2</Link>
-    <Link to="level3">Level 3</Link>
+    <MainNavbar />
+    <div className="items">
+      <Link to="level1" className="item">
+        <img src={level1} alt="" />
+        Level 1
+      </Link>
+      <Link to="level2" className="item">
+        <img src={level2} alt="" />
+        Level 2
+      </Link>
+      <Link to="level3" className="item">
+        <img src={level3} alt="" />
+        Level 3
+      </Link>
+    </div>
   </>
 );
 
