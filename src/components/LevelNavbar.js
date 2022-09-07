010e9675e44f4c7f0a29b2e24da603af100ac039
Kiernan Soriano <ksoriano10@horizon.csueastbay.edu>
2022-09-07 14:17:46 -0700

    Add character icons to navbar

--- src/components/LevelNavbar.js
+++ src/components/LevelNavbar.js
@@ -1,19 +1,42 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import Timer from "./Timer";
 import "../styles/LevelNavbar.css";
 import icon from "../assets/icon.png";
 
-const LevelNavbar = () => (
+const LevelNavbar = ({ characters }) => (
   <div className="level-navbar">
     <div className="nav-items">
       <Link to="/">
-        <img src={icon} alt="" />
+        <img className="icon" src={icon} alt="" />
       </Link>
-      {/* <Timer /> */}
+      <div className="character-icons">
+        {characters.map((character) => (
+          <img
+            key={character.id}
+            className={character.name}
+            src={require(`../assets/${character.image}`)}
+            alt=""
+          />
+        ))}
+      </div>
+
       <Link to="/">Return to home</Link>
     </div>
   </div>
 );
+
+LevelNavbar.propTypes = {
+  characters: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+      name: PropTypes.string,
+      x: PropTypes.number,
+      y: PropTypes.number,
+      image: PropTypes.string,
+    })
+  ).isRequired,
+};
 
 export default LevelNavbar;
