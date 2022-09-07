13b75d1c67b02185da59eb2836d13b9080074c47
Kiernan Soriano <ksoriano10@horizon.csueastbay.edu>
2022-09-07 15:09:20 -0700

    Create and style Mainpage Navbar

---
+++ src/components/MainNavbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import icon from "../assets/icon.png";
+import "../styles/MainNavbar.css";
+
+const MainNavbar = () => (
+  <div className="main-nav-items">
+    <img src={icon} alt="" />
+    <h1>Where&#39;s Waldo?</h1>
+  </div>
+);
+
+export default MainNavbar;
