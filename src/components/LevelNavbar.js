121694a849058c77491665e74d63157e731b01a8
Kiernan Soriano <ksoriano10@horizon.csueastbay.edu>
2022-09-07 12:57:38 -0700

    Add icon to navbar

--- src/components/LevelNavbar.js
+++ src/components/LevelNavbar.js
@@ -2,11 +2,15 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Timer from "./Timer";
 import "../styles/LevelNavbar.css";
+import icon from "../assets/icon.png";
 
 const LevelNavbar = () => (
   <div className="level-navbar">
     <div className="nav-items">
-      <Timer />
+      <Link to="/">
+        <img src={icon} alt="" />
+      </Link>
+      {/* <Timer /> */}
       <Link to="/">Return to home</Link>
     </div>
   </div>
